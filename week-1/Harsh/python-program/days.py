0f77d7a935fb0ba3ee563de574e7b75409527e87
harsh1waviz <harsh.waviz@gmail.com>
2020-07-22 01:22:05 +0530

    hot and cold days

---
+++ week-1/Harsh/python-program/days.py
@@ -0,0 +1,11 @@
+hot=True
+cold=False
+if hot:
+    print("it is a hot day")
+    print("drink plenty of water")
+elif cold:
+    print("it is a cold day")
+    print("wear warm cloths")
+else:
+    print("enjoy your day")
+                                  

b259c6c257a2e6391213d9d7b0e59a16be744df3
harsh1waviz <harsh.waviz@gmail.com>
2020-07-22 01:37:29 +0530

    logical operator program

---
+++ week-1/Harsh/python-program/compoperator.py
@@ -0,0 +1,10 @@
+name="john smith"
+if len(name)<3:
+    print("Name must be 3 character")
+elif len(name)>50:
+    print("Name must be maximum of  50 character")
+else:
+    print("Name look good")
+
+
+

d41441e830b135afc7edfd0b299afea4081cdd08
Arpit <Arpit.waviz@gmail.com>
2020-07-24 10:35:58 +0530

    Assignment

---
+++ week-1/Arpit/Assignment/sortWords.py
@@ -0,0 +1,11 @@
+string="hello everyonr , this is an example of sorted words"
+
+print(string)
+words= string.split()
+words.sort()
+print("the sorted words of this string are : ")
+for word in words:
+	print(word)
+
+
+

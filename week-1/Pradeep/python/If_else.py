7b8143d47c66dfe026d2e62726ca3bf2b2189567
pradeepwaviz <pradeep.waviz@gmail.com>
2020-07-26 11:21:17 +0530

    if else

---
+++ week-1/Pradeep/python/If_else.py
@@ -0,0 +1,10 @@
+is_hot=False
+is_cold=True
+if is_hot:
+    ptint("It is a hot day")
+    print("Drink a plenty of water")
+elif is_cold:
+    print("It is a cold day")
+    print("Wear warm clothes")
+else:
+    print("It is a lovely day")

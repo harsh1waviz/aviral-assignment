7dd8b5e2b4f18029fb1599803c9df1f627d03ca9
pradeepwaviz <pradeep.waviz@gmail.com>
2020-07-26 13:01:09 +0530

    nltk program

---
+++ week-1/Pradeep/nltk/first.py
@@ -0,0 +1,4 @@
+from nltk.tokenize import sent_tokenize,word_tokenize
+example_text="sir you helped me a lot so thank you so much"
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
---
+++ week-1/Pradeep/nltk/token.py
@@ -0,0 +1,5 @@
+from token import *
+from nltk.tokenize import sent_tokenize, word_tokenize
+example_text = "Hello how are you"
+print(sent_tokenize(example_text))
+print(word_tokenize(example_text))
